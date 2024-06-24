5d508c36b35c8be06659efce327d8002c7b2c549
Aryan Patel <144089245+aryanpatel0204@users.noreply.github.com>
2024-06-24 11:39:57 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CSS-JavaScript-Practice
+ This repository is made for practising JavaScript and CSS programs.
---
+++ TAILWIND/Danube/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      borderWidth: {
+        1: '1px',
+      },
+    },
+  },
+  plugins: [],
+}
+// borderWidth: {
+  // DEFAULT: '1px',
